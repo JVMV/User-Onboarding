98a0290e6f539cbb4cfea13c2a4db96f57314bb2
Justin Abellera <justin.c.abellera@gmail.com>
2022-07-29 15:26:26 -0700

    inputs testing

--- user-onboarding/cypress/e2e/form_test.cy.js
+++ user-onboarding/cypress/e2e/form_test.cy.js
@@ -17,8 +17,40 @@ describe('Form submits correctly', () => {
     // Objects are compared by reference!!!.
     expect({}).to.eql({});
     // Deep comparison
+
   })
 
+  it('the elements are showing', () => {
+    firstName().should('exist');
+    lastName().should('exist');
+    email().should('exist');
+    password().should('exist');
+    tos().should('exist');
+    submitBtn().should('exist');
+    cy.contains('Submit').should('exist');
+  })
+
+  describe('inputs can be filled out', () => {
+    it('all inputs work', () => {
+      firstName()
+        .should('have.value', '')
+        .type('Justin')
+        .should('have.value', 'Justin');
+      lastName()
+        .should('have.value', '')
+        .type('Abellera')
+        .should('have.value', 'Abellera');
+      email()
+        .should('have.value', '')
+        .type('email@email.com')
+        .should('have.value', 'email@email.com');
+      password()
+        .should('have.value', '')
+        .type('shh...secret')
+        .should('have.value', 'shh...secret');
+      tos().should('not.be.checked').check().should('be.checked');
+    })
+  })
 
 
 
